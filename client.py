5e726fb94af54dd34d097889cb23840c1f066fd2
Sakshi Konnur <skonnur@ucsc.edu>
2024-07-06 17:20:15 -0700

    pseudocoded

--- client.py
+++ client.py
@@ -2,3 +2,18 @@
 # this is the client side code uwu
 
 import socket
+import select
+import selectors
+import io
+
+
+# create a socket object
+
+# will reach out to the server first 
+server_ip = "127.0.0.1" # localhost, change when deployed 
+server_open_port = 6969
+# once connection is established 
+
+# send message 
+
+# receive message from the server 
